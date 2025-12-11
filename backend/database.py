1e276489439d944199664d8bd2b1f4155072cf47
Nikhil Dubey <nikhildubey183@gmail.com>
2025-12-11 23:55:51 +0530

    Initial commit: database changes

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,10 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from urllib.parse import quote_plus
 
-username = quote_plus('nikhildubey183')
-password = quote_plus('N1i2k3h4i5l6@')
+MONGO_URI = os.getenv("MONGODB_URI")
 
-MONGO_URI = f"mongodb+srv://{username}:{password}@cluster.mw4vj.mongodb.net/newAppDB?retryWrites=true&w=majority"
+if not MONGO_URI:
+    raise ValueError("❌ MONGODB_URI environment variable not set")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.get_default_database()
