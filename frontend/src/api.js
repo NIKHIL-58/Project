455826e1d078a2aa36daeef1486ae810482ddc83
Nikhil Dubey <nikhildubey183@gmail.com>
2025-12-16 01:05:17 +0530

    api update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // ✅ Deployed FastAPI backend URL
-const API_URL = "https://smarthire-backend-acen.onrender.com/";
+const API_URL = "https://smarthire-backend-acen.onrender.com";
 
 export const register = (username, password) => {
   return axios.post(`${API_URL}/register`, { username, password });
